f5b3072ec2cc0f11840eb659b8aadf94c8d81104
Vlad <mesey145@gmail.com>
2026-04-23 20:25:53 +0300

    feat: overall linter, prettier fixes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,16 @@
-import styles from './header.module.css'
+import styles from "./Header.module.css";
 import PlaceIcon from "../../assets/Icons/place.svg?react";
 
-function Header(){
-    return(
+function Header() {
+  return (
     <header className={styles.header}>
-        <h1 className={styles.logo}>Need for drive</h1>
-        <div className={styles.map}>
-            <PlaceIcon />
-            Ульяновск
-        </div>
+      <h1 className={styles.logo}>Need for drive</h1>
+      <div className={styles.map}>
+        <PlaceIcon />
+        Ульяновск
+      </div>
     </header>
-    );
+  );
 }
 
-export default Header
+export default Header;
